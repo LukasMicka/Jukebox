ea7f056365b9567ec290c0fae7d57a76d3da1d41
LukasMicka <lukas.micka@seznam.cz>
2019-02-15 10:26:59 +0100

    first commit

---
+++ Jukebox/resources/views/jukebox.php
@@ -0,0 +1,36 @@
+<form action="" method="post">
+
+<label for="">Author:</label><br>
+<input type="text" name="author" value="">
+<br>
+<br>
+
+<label for="">Song:</label><br>
+<input type="text" name="song" value="">
+<br>
+<br>
+
+<label for="">Code:</label><br>
+<input type="text" name="code" value="">
+<br>
+<br>
+
+<label for="">URL:</label><br>
+<input type="text" name="url" value="">
+<br>
+<br>
+
+<label for="">Song:</label><br>
+<input type="text" name="song" value="">
+<br>
+<br>
+
+<label for="">Added at:</label><br>
+<input type="text" name="added" value="">
+<br>
+<br>
+
+<input type="submit" value="Submit song">
+<br>
+
+</form>
